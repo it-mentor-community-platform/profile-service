0c39fdd29861aa38899c3839b7641b8b4320c142
Dmitry 0xErr <syncline0x@gmail.com>
2026-01-04 23:49:56 +1000

    feat: добавление личной информации пользователя в детали профиля

--- src/main/java/com/itmentorcommunityplatform/profileservice/service/ProfileService.java
+++ src/main/java/com/itmentorcommunityplatform/profileservice/service/ProfileService.java
@@ -20,7 +20,6 @@ import com.itmentorcommunityplatform.profileservice.validator.registry.ProfileDe
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -41,26 +40,43 @@ public class ProfileService {
     private final GithubProfileUrlValidator githubProfileUrlValidator;
 
     @Transactional
-    public void createProfile(UserCreatedEvent event) {
+    public void createOrUpdateProfile(UserCreatedEvent event) {
         if (event == null || event.getTelegramUserId() == null) {
             log.warn("Received empty event or null telegramUserId. Skipping.");
             return;
         }
+
         Long telegramUserId = event.getTelegramUserId();
         log.info("Attempting to create profile for telegramUserId: {}", telegramUserId);
 
-        if (profileRepository.findByTelegramUserId(telegramUserId).isPresent()) {
-            log.info("Profile for telegramUserId: {} already exists. Skipping creation.", telegramUserId);
-            return;
-        }
-        Profile newProfile = Profile.builder()
-                .id(null)
-                .telegramUserId(telegramUserId)
-                .details(new HashSet<>())
-                .build();
+        Map<String, String> maybeProfileInfo = getIdentityInfoIfPresent(event);
 
-        profileRepository.save(newProfile);
-        log.info("Successfully created profile with telegramUserId: {}", telegramUserId);
+        Optional<Profile> maybeProfile = profileRepository.findByTelegramUserId(telegramUserId);
+
+        if (maybeProfile.isPresent()) {
+            log.info("Profile for telegramUserId: {} exists. Updating details.", telegramUserId);
+            Profile existingProfile = maybeProfile.get();
+
+            Set<ProfileDetail> profileDetails = mergeProfileDetails(existingProfile.getDetails(), maybeProfileInfo);
+            existingProfile.setDetails(profileDetails);
+
+            profileRepository.save(existingProfile);
+        } else {
+            log.info("Creating new profile for telegramUserId: {}", telegramUserId);
+
+            Set<ProfileDetail> newProfileDetails = maybeProfileInfo.entrySet().stream()
+                    .map(e -> new ProfileDetail(e.getKey(), e.getValue()))
+                    .collect(Collectors.toSet());
+
+            Profile newProfile = Profile.builder()
+                    .id(null)
+                    .telegramUserId(telegramUserId)
+                    .details(newProfileDetails)
+                    .build();
+
+            profileRepository.save(newProfile);
+            log.info("Successfully created profile with telegramUserId: {}", telegramUserId);
+        }
     }
 
     public ProfileDetailsResponseDto getCurrentUserProfile(Long telegramUserId) {
@@ -239,5 +255,17 @@ public class ProfileService {
         List<Achievement> achievements = achievementRepository.findAllByProfileIdAndPubliclyVisibleTrue(profileId);
 
         return mapToProfileDetailDto(profile.getDetails(), achievements);
+    }
+
+    private Map<String, String> getIdentityInfoIfPresent(UserCreatedEvent event) {
+        Map<String, String> details = new HashMap<>();
+
+        if (event.getFirstName() != null && !event.getFirstName().isBlank()) {
+            details.put(ProfileDetailType.FIRST_NAME.getDetailName(), event.getFirstName());
+        }
+        if (event.getLastName() != null && !event.getLastName().isBlank()) {
+            details.put(ProfileDetailType.LAST_NAME.getDetailName(), event.getLastName());
+        }
+        return details;
     }
 }
