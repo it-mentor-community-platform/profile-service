c146cdb980998c882e7fab50eadbadeb97097dcb
Dmitry 0xErr <syncline0x@gmail.com>
2026-01-28 18:59:58 +1000

    refactor: изменены названия полей с camelCase на snake_case для топика projects.project.created

--- src/main/java/com/itmentorcommunityplatform/profileservice/dto/event/ProjectCreatedEvent.java
+++ src/main/java/com/itmentorcommunityplatform/profileservice/dto/event/ProjectCreatedEvent.java
@@ -1,5 +1,6 @@
 package com.itmentorcommunityplatform.profileservice.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,11 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class ProjectCreatedEvent {
+    @JsonProperty("author_telegram_user_id")
     Long authorTelegramUserId;
+    @JsonProperty("author_telegram_profile_url")
     String authorTelegramProfileUrl;
+    @JsonProperty("github_repository_url")
     String githubRepositoryUrl;
+    @JsonProperty("programming_language")
     String programmingLanguage;
+    @JsonProperty("roadmap_project")
     String roadmapProject;
+    @JsonProperty("added_timestamp")
     Long addedTimestamp;
+    @JsonProperty("project_source_type")
     String projectSourceType;
 }
