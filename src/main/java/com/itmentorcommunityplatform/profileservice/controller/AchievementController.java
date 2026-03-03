974f8c53a77d719c2d036c9e4effe2153a190f1a
Artemii_Khokhlov <artemii.hhlov@gmail.com>
2026-03-03 15:02:49 +0600

    fix: change uri for profile-service

--- src/main/java/com/itmentorcommunityplatform/profileservice/controller/AchievementController.java
+++ src/main/java/com/itmentorcommunityplatform/profileservice/controller/AchievementController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/profile")
 @RequiredArgsConstructor
 public class AchievementController {
 
--- src/main/java/com/itmentorcommunityplatform/profileservice/controller/ProfileController.java
+++ src/main/java/com/itmentorcommunityplatform/profileservice/controller/ProfileController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/profile")
+@RequestMapping("/api/profile/profile")
 @RequiredArgsConstructor
 public class ProfileController {
 
